1bb257352bb2d67612a7d283b80c4276505cd005
ashleyfontes <90660414+ashleyfontes@users.noreply.github.com>
2021-12-13 20:27:50 -0500

    Test MySQL & Python Connection

---
+++ TestScript.sql
@@ -0,0 +1,4 @@
+SELECT * FROM FinalprojectFE595Group7.ArticleData;
+
+INSERT INTO ArticleData (author,title,subject,month,day,year,source,link) VALUES ('Author', 'Title', 'Subject', 12, 22, 2001, 'Source', 'Link');
+DELETE FROM ArticleData where author = 'Author';
