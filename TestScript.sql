f8777544934b5db4fcf8b5dc1b295f232626c175
ashleyfontes <90660414+ashleyfontes@users.noreply.github.com>
2021-12-19 22:31:37 -0500

    Delete TestScript.sql

--- TestScript.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM FinalprojectFE595Group7.ArticleData;
-
-INSERT INTO ArticleData (author,title,subject,month,day,year,source,link) VALUES ('Author', 'Title', 'Subject', 12, 22, 2001, 'Source', 'Link');
-DELETE FROM ArticleData where author = 'Author';
